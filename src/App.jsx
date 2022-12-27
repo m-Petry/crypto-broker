905edc8f9c446a557e8b3e062284ac82a15c04d8
Marcelo Petry <marcelospetry@gmail.com>
2022-12-27 12:02:36 -0300

    test: testing mui functions on header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header";
 import Homepage from "./Pages/Homepage";
+import CoinPage from "./Pages/CoinPage";
+import { Box } from "@mui/system";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <Route path="/" component={Homepage} />
-      </div>
+      <Box
+        bgcolor="#14161a"
+        color="white"
+        minHeight="100vh"
+        display="flex"
+        flexDirection="row"
+        minWidth={window.innerWidth}
+      >
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/coins/:id" element={<CoinPage />} />
+          </Routes>
+        </div>
+      </Box>
     </BrowserRouter>
   );
 }
-
 export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,52 @@
+import {
+  AppBar,
+  Box,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography
+} from "@mui/material";
+import { Container } from "@mui/system";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  // Creates the route for homepage
+  const navigate = useNavigate();
+
+  return (
+    <AppBar color="transparent" position="static">
+      <Container>
+        <Toolbar>
+          <Typography
+            style={{
+              flex: 1,
+              color: "gold",
+              fontFamily: "Montserrat",
+              fontWeight: "bold",
+              cursor: "pointer"
+            }}
+            onClick={() => navigate("/")}
+          >
+            Crypto Broker
+          </Typography>
+          <Select
+            variant="outlined"
+            style={{
+              width: 100,
+              height: 40,
+              marginLeft: 15,
+              cursor: "pointer"
+            }}
+            value={"USD"}
+          >
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"BRL"}>BRL</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Header;
