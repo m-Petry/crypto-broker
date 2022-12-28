7021a8e59c3f77bb9096c9a0c4e4104be7b54469
Marcelo Petry <marcelospetry@gmail.com>
2022-12-28 20:31:28 -0300

    Build: tailwind added

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -3,14 +3,17 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { TrendingCoins } from "../Config/Api";
 import { CryptoState } from "../CryptoContext";
+import AliceCarousel from "react-alice-carousel";
+import { Link } from "react-router-dom";
 
 const Carousel = () => {
   const [trending, setTrending] = useState([]);
-  const { currency } = CryptoState();
+  const { currency, symbol } = CryptoState();
 
   const fetchTrendingCoins = async () => {
     const { data } = await axios.get(TrendingCoins(currency));
 
+    console.log(data);
     setTrending(data);
   };
 
@@ -20,6 +23,42 @@ const Carousel = () => {
     fetchTrendingCoins();
   }, [currency]);
 
+  const items = trending.map((coin) => {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          cursor: "pointer",
+          textTransform: "uppercase",
+          color: "white"
+        }}
+      >
+        <Link to={`/coins/${coin.id}`}>
+          <img
+            src={coin?.image}
+            alt={coin.name}
+            height="80"
+            // width="50"
+            style={{
+              marginBottom: 10
+            }}
+          />
+        </Link>
+      </Box>
+    );
+  });
+
+  const responsive = {
+    0: {
+      items: 2
+    },
+    512: {
+      items: 4
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -28,7 +67,17 @@ const Carousel = () => {
         alignItems: "center"
       }}
     >
-      <div>Carousel</div>;
+      <AliceCarousel
+        mouseTracking
+        infinite
+        autoPlayInterval={1000}
+        animationDuration={1500}
+        disableDotsControls
+        disableButtonsControls
+        responsive={responsive}
+        items={items}
+        autoPlay
+      />
     </Box>
   );
 };
